8d5c6889daefddab03e9d604a2e8de6a54abdab9
hayato <hayashi.hyt2001@icloud.com>
2024-12-01 08:53:25 +0900

    feat: Implement camera

--- beamng/pub/camera.py
+++ beamng/pub/camera.py
@@ -1,18 +1,23 @@
 import time
 
-def send_camera_data(data_publisher, camera, stop_event):
-  camera_hz = 30
+from shared import *
+
+def send_camera_data(camera):
+  data_publisher = DataPublisherSingleton()
+  stop_event_instance = StopEventSingleton()
+  
+  camera_hz = 50
   camera_interval = 1.0 / camera_hz
   base_time = time.time()
   
   while True:
-    if stop_event.is_set():
+    if stop_event_instance.get_value():
       break
     
     data = camera.stream_raw()
     image_data = data['colour'].tobytes()
     
-    data_publisher.process_camera(image_data)
+    data_publisher.camera(image_data)
     
     next_time = max(0, camera_interval - (time.time() - base_time))
     time.sleep(next_time)
