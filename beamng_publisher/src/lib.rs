2ed6391a2874ae3a94a8719c46a2c85a60129fa2
hayato <hayashi.hyt2001@icloud.com>
2024-12-01 09:05:24 +0900

    refactor: Create publihsers only once in the constructor

--- beamng_publisher/src/lib.rs
+++ beamng_publisher/src/lib.rs
@@ -3,6 +3,7 @@ use pyo3::types::PyBytes;
 use cdr::{CdrLe, Infinite};
 use std::time::{SystemTime, UNIX_EPOCH};
 use zenoh::{prelude::*, Config, Session};
+use zenoh::pubsub::Publisher;
 use zenoh_ros_type::{
     builtin_interfaces,
     std_msgs,
@@ -13,6 +14,7 @@ use zenoh_ros_type::{
 };
 use numpy::{PyArray2};
 use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
 
 #[derive(Serialize, Deserialize, PartialEq, Clone)]
 pub struct BatteryStatus {
@@ -22,7 +24,18 @@ pub struct BatteryStatus {
 
 #[pyclass(module = "beamng_publisher")]
 struct BeamngPublisher {
-    session: Session
+    session: Arc<Session>,
+    clock_publisher: Arc<Mutex<Publisher<'static>>>,
+    pointcloud_publisher: Arc<Mutex<Publisher<'static>>>,
+    imu_publisher: Arc<Mutex<Publisher<'static>>>,
+    camera_publisher: Arc<Mutex<Publisher<'static>>>,
+    velocity_publisher: Arc<Mutex<Publisher<'static>>>,
+    steering_publisher: Arc<Mutex<Publisher<'static>>>,
+    gear_publisher: Arc<Mutex<Publisher<'static>>>,
+    control_mode_publisher: Arc<Mutex<Publisher<'static>>>,
+    battery_publisher: Arc<Mutex<Publisher<'static>>>,
+    hazard_publisher: Arc<Mutex<Publisher<'static>>>,
+    turn_signal_publisher: Arc<Mutex<Publisher<'static>>>
 }
 
 #[pymethods]
@@ -33,17 +46,91 @@ impl BeamngPublisher {
             .expect("Unable to load configuration");
         
         let session = zenoh::open(config).wait().expect("Unable to open session");
+        let session = Arc::new(session);
+
+        let clock_publisher = session
+            .declare_publisher("clock")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let clock_publisher = Arc::new(Mutex::new(clock_publisher));
+
+        let pointcloud_publisher = session
+            .declare_publisher("sensing/lidar/concatenated/pointcloud")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let pointcloud_publisher = Arc::new(Mutex::new(pointcloud_publisher));
+
+        let imu_publisher = session
+            .declare_publisher("imu/data")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let imu_publisher = Arc::new(Mutex::new(imu_publisher));
+
+        let camera_publisher = session
+            .declare_publisher("sensing/camera")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let camera_publisher = Arc::new(Mutex::new(camera_publisher));
+
+        let velocity_publisher = session
+            .declare_publisher("vehicle/status/velocity_status")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let velocity_publisher = Arc::new(Mutex::new(velocity_publisher));
+
+        let steering_publisher = session
+            .declare_publisher("vehicle/status/steering_status")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let steering_publisher = Arc::new(Mutex::new(steering_publisher));
+
+        let gear_publisher = session
+            .declare_publisher("vehicle/status/gear_status")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let gear_publisher = Arc::new(Mutex::new(gear_publisher));
+
+        let control_mode_publisher = session
+            .declare_publisher("vehicle/status/control_mode")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let control_mode_publisher = Arc::new(Mutex::new(control_mode_publisher));
+
+        let battery_publisher = session
+            .declare_publisher("vehicle/status/battery_charge")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let battery_publisher = Arc::new(Mutex::new(battery_publisher));
+
+        let hazard_publisher = session
+            .declare_publisher("vehicle/status/hazard_lights_status")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let hazard_publisher = Arc::new(Mutex::new(hazard_publisher));
+
+        let turn_signal_publisher = session
+            .declare_publisher("vehicle/status/turn_indicators_status")
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let turn_signal_publisher = Arc::new(Mutex::new(turn_signal_publisher));
         
         Ok(BeamngPublisher {
-            session
+            session,
+            clock_publisher,
+            pointcloud_publisher,
+            imu_publisher,
+            camera_publisher,
+            velocity_publisher,
+            steering_publisher,
+            gear_publisher,
+            control_mode_publisher,
+            battery_publisher,
+            hazard_publisher,
+            turn_signal_publisher,
         })
     }
 
     fn publish_clock(&self) -> PyResult<()> {
-        let publisher = self.session.declare_publisher("clock")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
-
         let now = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("Unable to get current time");
@@ -56,7 +143,11 @@ impl BeamngPublisher {
         let clock_msgs = rosgraph_msgs::Clock { clock: time };
         let encoded = cdr::serialize::<_, _, CdrLe>(&clock_msgs, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
-        publisher.put(encoded)
+
+        let mut publisher = self.clock_publisher.lock().unwrap();
+
+        publisher
+            .put(encoded)
             .wait()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
 
@@ -64,13 +155,7 @@ impl BeamngPublisher {
     }
 
     fn process_pointcloud(&self, pointcloud: &PyArray2<f32>) -> PyResult<()> {  
-        let publisher = self.session.declare_publisher("sensing/lidar/concatenated/pointcloud")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
-
-        // let publisher = self.session.declare_publisher("sensing/lidar/top/pointcloud_raw_ex")
-        //     .wait()
-        //     .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let mut publisher = self.pointcloud_publisher.lock().unwrap();
     
         let points: Vec<f32> = unsafe {
             pointcloud.as_slice().unwrap().to_vec()
@@ -181,7 +266,8 @@ impl BeamngPublisher {
         let encoded = cdr::serialize::<_, _, CdrLe>(&pointcloud2, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
     
-        publisher.put(encoded)
+        publisher
+            .put(encoded)
             .wait()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
     
@@ -189,9 +275,7 @@ impl BeamngPublisher {
     }
 
     fn process_imu(&self, imu_data: Vec<f64>) -> PyResult<()> {
-        let publisher = self.session.declare_publisher("imu/data")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let mut publisher = self.imu_publisher.lock().unwrap();
 
         let now = SystemTime::now()
             .duration_since(UNIX_EPOCH)
@@ -233,7 +317,8 @@ impl BeamngPublisher {
         let encoded = cdr::serialize::<_, _, CdrLe>(&imu_msg, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
 
-        publisher.put(encoded)
+        publisher
+            .put(encoded)
             .wait()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
 
@@ -241,9 +326,7 @@ impl BeamngPublisher {
     }
 
     fn process_camera(&self, data: &PyBytes) -> PyResult<()> {
-        let publisher = self.session.declare_publisher("sensing/camera")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        let mut publisher = self.camera_publisher.lock().unwrap();
 
         let now = SystemTime::now()
             .duration_since(UNIX_EPOCH)
@@ -277,7 +360,8 @@ impl BeamngPublisher {
         let encoded = cdr::serialize::<_, _, CdrLe>(&image, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
 
-        publisher.put(encoded)
+        publisher
+            .put(encoded)
             .wait()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
 
@@ -296,34 +380,7 @@ impl BeamngPublisher {
         hazard: u8,
         turn_signal: u8,
     ) -> PyResult<()> {
-        let velocity_pub = self.session.declare_publisher("vehicle/status/velocity_status")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
-
-        let steering_pub = self.session.declare_publisher("vehicle/status/steering_status")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
-
-        let gear_pub = self.session.declare_publisher("vehicle/status/gear_status")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
-
-        let control_mode_pub = self.session.declare_publisher("vehicle/status/control_mode")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
-
-        let battery_pub = self.session.declare_publisher("vehicle/status/battery_charge")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
-
-        let hazard_pub = self.session.declare_publisher("vehicle/status/hazard_lights_status")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
-
-        let turn_signal_pub = self.session.declare_publisher("vehicle/status/turn_indicators_status")
-            .wait()
-            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
-
+        let mut velocity_pub = self.velocity_publisher.lock().unwrap();
         let velocity_msgs = {
             let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get current time");
             let time = builtin_interfaces::Time {
@@ -350,10 +407,12 @@ impl BeamngPublisher {
         };
         let serialized_velocity = cdr::serialize::<_, _, CdrLe>(&velocity_msgs, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
-        velocity_pub.put(serialized_velocity)
+        velocity_pub
+            .put(serialized_velocity)
             .wait()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
 
+        let mut steering_pub = self.steering_publisher.lock().unwrap();
         let steering_msgs = {
             let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get current time");
             let time = builtin_interfaces::Time {
@@ -374,11 +433,13 @@ impl BeamngPublisher {
         };
         let serialized_steering = cdr::serialize::<_, _, CdrLe>(&steering_msgs, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
-        steering_pub.put(serialized_steering)
+        steering_pub
+            .put(serialized_steering)
             .wait()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
         
 
+        let mut gear_pub = self.gear_publisher.lock().unwrap();
         let gear_msgs = {
             let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get current time");
             let time = builtin_interfaces::Time {
@@ -392,10 +453,12 @@ impl BeamngPublisher {
         };
         let serialized_gear = cdr::serialize::<_, _, CdrLe>(&gear_msgs, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
-        gear_pub.put(serialized_gear)
-        .wait()
-        .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
+        gear_pub
+            .put(serialized_gear)
+            .wait()
+            .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
 
+        let mut control_mode_pub = self.control_mode_publisher.lock().unwrap();
         let control_mode_msgs = {
             let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get current time");
             let time = builtin_interfaces::Time {
@@ -409,10 +472,12 @@ impl BeamngPublisher {
         };
         let serialized_control_mode = cdr::serialize::<_, _, CdrLe>(&control_mode_msgs, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
-        control_mode_pub.put(serialized_control_mode)
+        control_mode_pub
+            .put(serialized_control_mode)
             .wait()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
 
+        let mut battery_pub = self.battery_publisher.lock().unwrap();
         let battery_msgs = {
             let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get current time");
             let time = builtin_interfaces::Time {
@@ -426,10 +491,12 @@ impl BeamngPublisher {
         };
         let serialized_battery = cdr::serialize::<_, _, CdrLe>(&battery_msgs, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
-        battery_pub.put(serialized_battery)
+        battery_pub
+            .put(serialized_battery)
             .wait()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
 
+        let mut hazard_pub = self.hazard_publisher.lock().unwrap();
         let hazard_msgs = {
             let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get current time");
             let time = builtin_interfaces::Time {
@@ -443,10 +510,12 @@ impl BeamngPublisher {
         };
         let serialized_hazard = cdr::serialize::<_, _, CdrLe>(&hazard_msgs, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
-        hazard_pub.put(serialized_hazard)
+        hazard_pub
+            .put(serialized_hazard)
             .wait()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
 
+        let mut turn_signal_pub = self.turn_signal_publisher.lock().unwrap();
         let turn_signal_msgs = {
             let now = SystemTime::now().duration_since(UNIX_EPOCH).expect("Unable to get current time");
             let time = builtin_interfaces::Time {
@@ -460,7 +529,8 @@ impl BeamngPublisher {
         };
         let serialized_turn_signal = cdr::serialize::<_, _, CdrLe>(&turn_signal_msgs, Infinite)
             .map_err(|err| pyo3::exceptions::PyException::new_err(err.to_string()))?;
-        turn_signal_pub.put(serialized_turn_signal)
+        turn_signal_pub
+            .put(serialized_turn_signal)
             .wait()
             .map_err(|e| pyo3::exceptions::PyRuntimeError::new_err(e.to_string()))?;
     
