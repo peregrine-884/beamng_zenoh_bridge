9b81ae92762b3dc2375bcb5a9cd904b59e5425f5
hayato <hayashi.hyt2001@icloud.com>
2025-03-03 11:56:23 +0900

    refactor

--- beamng/calibration/accel.py
+++ beamng/calibration/accel.py
@@ -4,95 +4,59 @@ import time
 def accel_map():
   vehicle_instance = VehicleSingleton()
   vehicle_state_instance = VehicleStateSingleton()
-  stop_event_instance = StopEventSingleton()
   
   num_loops = 150
-  exit_loop = False  # ループを抜けるためのフラグ
   flag = False
   
-  accels = [round(i * 0.05, 2) for i in range(1, 21)]
-  accels.reverse()
-  
+  accels = [round(i * 0.05, 2) for i in range(1, 21)][::-1]
   vehicle_state_instance.set_manual_mode(False)
 
   for accel in accels:
-      print(f"================== Accelerating with {accel} ==================")
-  
-      time.sleep(3)
-      
-      for i in range(num_loops):
-          print(f"================== Loop {i} ==================")
+    print(f"================== Accelerating with {accel} ==================")
+    time.sleep(3)
+    
+    for i in range(num_loops):
+      print(f"================== Loop {i} ==================")
+      time.sleep(2)
 
-          time.sleep(2)
-          vehicle_instance.vehicle.control(
-            gear = 2,
-            parkingbrake = 0.0,
+      vehicle_instance.vehicle.control(gear=2, parkingbrake=0.0)
+      set_control_executed = False
+
+      while True:  
+        if vehicle_state_instance.get_manual_mode():
+          vehicle_instance.set_control(steering=0.0, throttle=0.0, brake=0.0)
+          vehicle_instance.vehicle.teleport(
+            pos=(-1020.482, 0.000, 0.600),
+            rot_quat=(0.0, 0.0, -0.70710678, 0.70710678),
+            reset=True
           )
-          
-          set_control_executed = False  # `set_control` が実行されたかを示すフラグ
-          while True:
-              if stop_event_instance.get_value():  # stop_event_instanceがTrueならループ終了
-                  exit_loop = True
-                  break
-              
-              if vehicle_state_instance.get_manual_mode():
-                  vehicle_instance.set_control(
-                      steering=0.0,
-                      throttle=0.0,
-                      brake=0.0
-                  )
-                  
-                  vehicle_instance.vehicle.teleport(
-                      pos=(-1020.482, 0.000, 0.600), 
-                      rot_quat=(0.0, 0.0, -0.70710678, 0.70710678), 
-                      reset=True
-                  )
-                  
-                  vehicle_state_instance.set_manual_mode(False)
-                  
-                  flag = True
-                  break
-
-              state = vehicle_instance.get_state()
-              if state is None:
-                  continue
-
-              position = state["pos"]
-              velocity = vehicle_state_instance.get_longitudinal_vel()
-
-              if not set_control_executed:
-                  # 一度だけ `set_control` を実行
-                  vehicle_instance.set_control(
-                      steering=0.0,
-                      throttle=accel,
-                      brake=0.0
-                  )
-                  set_control_executed = True  # 実行済みにする
-                  print("Control command executed.")
-
-              if (position[0] > 500) or (velocity > 30.0):
-                  vehicle_instance.set_control(
-                      steering=0.0,
-                      throttle=0.0,
-                      brake=0.0
-                  )
-                  
-                  vehicle_instance.vehicle.teleport(
-                      pos=(-1020.482, 0.000, 0.600), 
-                      rot_quat=(0.0, 0.0, -0.70710678, 0.70710678), 
-                      reset=True
-                  )
-                  break
-
-          if exit_loop:  # 内側のforループも終了
-              break
-          
-          if flag:
-              flag = False
-              break
-
-      if exit_loop:  # 最外のforループも終了
+          vehicle_state_instance.set_manual_mode(False)
+          flag = True
           break
 
-  print("All loops exited.")
+        state = vehicle_instance.get_state()
+        if state is None:
+          continue
 
+        position = state["pos"]
+        velocity = vehicle_state_instance.get_longitudinal_vel()
+
+        if not set_control_executed:
+          vehicle_instance.set_control(steering=0.0, throttle=accel, brake=0.0)
+          set_control_executed = True
+          print("Control command executed.")
+
+        if position[0] > 500 or velocity > 30.0:
+          vehicle_instance.set_control(steering=0.0, throttle=0.0, brake=0.0)
+          vehicle_instance.vehicle.teleport(
+            pos=(-1020.482, 0.000, 0.600),
+            rot_quat=(0.0, 0.0, -0.70710678, 0.70710678),
+            reset=True
+          )
+          break
+
+      if flag:
+        flag = False
+        break
+
+  print("All loops exited.")
--- beamng/create_accel_brake_map.py
+++ beamng/create_accel_brake_map.py
@@ -1,88 +1,77 @@
 import random
+import threading
+import keyboard
+import zenoh
 from beamngpy import BeamNGpy, Scenario, Vehicle, set_up_simple_logging
 from beamngpy.sensors import Electrics
-import keyboard
-import threading
-import zenoh_bridge
-import zenoh
-
 from shared import *
-
 from pub.clock import send_clock_data
 from pub.vehicle_control import send_vehicle_control_data
 from pub.vehicle_info import send_vehicle_info_data
 from sub.model_control import model_control_callback
 from calibration.calibration import create_accel_brake_map
+import zenoh_bridge
 
 def main():
   random.seed(1703)
   set_up_simple_logging()
-  
+
   beamng = BeamNGpy('localhost', 64256)
   bng = beamng.open(launch=False)
-  
+
   scenario = Scenario('tech_ground', 'LiDAR_demo', description='Spanning the map with a LiDAR sensor')
   vehicle = Vehicle('ego_vehicle', model='etk800', license='RED', color='Blue')
-  scenario.add_vehicle(vehicle,
-    pos=(-1020.482, 0.000, 1.000), rot_quat=(0.0, 0.0, -0.70710678, 0.70710678)
+
+  scenario.add_vehicle(
+    vehicle,
+    pos=(-1020.482, 0.000, 1.000),
+    rot_quat=(0.0, 0.0, -0.70710678, 0.70710678)
   )
-  
+
   scenario.make(bng)
-  
+
   bng.settings.set_deterministic(60)
   bng.scenario.load(scenario)
   bng.ui.hide_hud()
   bng.scenario.start()
-  
+
   vehicle.sensors.attach('electrics', Electrics())
-  
+
   config = zenoh.Config.from_file("C:\\Users\\hayat\\zenoh_beamng_bridge\\config\\beamng-conf.json5")
   session = zenoh.open(config)
-  # key = 'rate_limitted/model/vehicle/command/actuation_cmd'
-  # model_vehicle_control_sub = session.declare_subscriber(key, model_control_callback)
-  
+
   stop_event = threading.Event()
-  stop_thread = threading.Thread(target=lambda: keyboard.wait('q') or stop_event.set())
-  
+  threading.Thread(target=lambda: keyboard.wait('q') or stop_event.set()).start()
+
   data_publisher_instance = DataPublisherSingleton()
   data_publisher_instance.set_data_publisher(zenoh_bridge.BeamngDataPublisher())
-  
+
   vehicle_instance = VehicleSingleton()
   vehicle_instance.set_vehicle(vehicle)
-  
+
   stop_event_instance = StopEventSingleton()
   stop_event_instance.set_stop_event(stop_event)
-  
+
   vehicle_state_instance = VehicleStateSingleton()
   
-  keyboard.on_press_key('s', 
-        lambda event: vehicle_state_instance.set_manual_mode(not vehicle_state_instance.get_manual_mode()))
-  
-  clock_thread = threading.Thread(target=send_clock_data)
-  vehicle_control_thread = threading.Thread(target=send_vehicle_control_data)
-  vehicle_info_thread = threading.Thread(target=send_vehicle_info_data)
-  get_vehicle_data_thread = threading.Thread(target=get_sensor_data)
-  
-  accel_brake_thread = threading.Thread(target=create_accel_brake_map)
-  
-  stop_thread.start()
-  clock_thread.start()
-  vehicle_control_thread.start()
-  vehicle_info_thread.start()
-  get_vehicle_data_thread.start()
-  
-  accel_brake_thread.start()
-  
-  stop_thread.join()
-  clock_thread.join()
-  vehicle_control_thread.join()
-  vehicle_info_thread.join()
-  get_vehicle_data_thread.join()
-  
-  accel_brake_thread.join()
-  
-  # model_vehicle_control_sub.undeclare()
+  keyboard.on_press_key('s', lambda event: vehicle_state_instance.set_manual_mode(
+    not vehicle_state_instance.get_manual_mode()
+  ))
+
+  threads = [
+    threading.Thread(target=send_clock_data),
+    threading.Thread(target=send_vehicle_control_data),
+    threading.Thread(target=send_vehicle_info_data),
+    threading.Thread(target=get_sensor_data),
+    threading.Thread(target=create_accel_brake_map)
+  ]
+
+  for thread in threads:
+    thread.start()
+  for thread in threads:
+    thread.join()
+
   session.close()
-  
+
 if __name__ == '__main__':
   main()
