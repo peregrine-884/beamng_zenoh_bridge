5baf9233e1029556752a6dda023f6c7e2b4aa98c
hayato <hayashi.hyt2001@icloud.com>
2025-03-06 11:57:34 +0900

    refactor

--- core/utils/config_loader.py
+++ core/utils/config_loader.py
@@ -1,29 +1,52 @@
+import os
 import json5
 
-def load_config_from_json5(json5_path):
-  with open(json5_path, 'r') as f:
-    return json5.load(f)
+def load_config_from_json5(config_dir, config_path):
+  config_path = os.path.join(config_dir, config_path)
+  print(f"Loading config from {config_path}")
   
+  if not os.path.exists(config_path):
+    raise FileNotFoundError(f"Config file not found at {config_path}")
+  
+  with open(config_path, "r") as f:
+    data = json5.load(f)
 
-def print_formatted_data(data):
-  for key, value in data.items():
-    print(f"=== {key.upper()} ===")
-    
-    if isinstance(value, list):
-      if len(value) == 0:
-        print("  (empty)")
-      for item in value:
-        if isinstance(item, dict) and "name" in item:
-          print(f"- {item['name']}")
-        for sub_key, sub_value in item.items():
-          print(f"  {sub_key}: {sub_value}")
-        print("-" * 50)
-    
-    elif isinstance(value, dict):
-      for sub_key, sub_value in value.items():
-        print(f"  {sub_key}: {sub_value}")
-    
-    else:
-      print(f"  {value}")
+  if "map" in data:
+    map_path = os.path.join(config_dir, data["map"])
+    map_data = load_config_from_json5(config_dir, data["map"])
+    data["map_data"] = map_data
 
-    print("\n")
+  if "sensors" in data:
+    for sensor_type, sensor_file in data["sensors"].items():
+      sensor_file_path = os.path.join(config_dir, sensor_file)
+      print(f"{sensor_type.capitalize()} sensor file path: {sensor_file_path}")
+      sensor_data = load_config_from_json5(config_dir, sensor_file)
+      data[sensor_type] = sensor_data
+  
+  if "spawn_points" in data:
+    spawn_file = data["spawn_points"]
+    spawn_file_path = os.path.join(config_dir, spawn_file)
+    print(f"Spawn point file path: {spawn_file_path}")
+    spawn_data = load_config_from_json5(config_dir, spawn_file_path)
+    data["spawn_points_data"] = spawn_data
+
+  return data
+
+def split_data_from_config(data):
+    level_and_description = {
+        'level': data['map_data']['level'],
+        'description': data['map_data']['description']
+    }
+
+    ego_and_npc_vehicles = {
+        'ego_vehicle': data['map_data']['spawn_points_data']['ego_vehicle'],
+        'npc_vehicles': data['map_data']['spawn_points_data']['npc_vehicles']
+    }
+
+    sensors = {
+        'cameras': data['cameras'],
+        'imus': data['imus'],
+        'lidars': data['lidars']
+    }
+
+    return level_and_description, ego_and_npc_vehicles, sensors
