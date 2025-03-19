2dc5a3061584a24d3625f4295a5e43ac3249892f
peregrine-884 <hayashi.hyt2001@icloud.com>
2025-03-20 08:20:40 +0900

    fix

--- zenoh_bridge/setup.py
+++ zenoh_bridge/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(name='zenoh_bridge',
-  version='0,1',
+  version='0.1',
   rust_extensions=[
     RustExtension('zenoh_bridge', 'Cargo.toml',
                   binding=Binding.PyO3)],
